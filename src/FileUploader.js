0ff81c0b5eae1f5eb44f8b0bbe2f9b835d960998
Verena Girgis <verenagirgis22@gmail.com>
2023-02-21 23:44:56 -0800

    file upload functionality

---
+++ src/FileUploader.js
@@ -0,0 +1,50 @@
+import AWS from 'aws-sdk';
+import { useState } from 'react';
+
+AWS.config.update({
+  accessKeyId: 'AKIAWKMQZAIBRTJXARUV',
+  secretAccessKey: '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',
+  region: 'us-east-1',
+  signatureVersion: 'v4',
+});
+
+export default function ImageUploader() {
+    const s3 = new AWS.S3();
+    const [imageUrl, setImageUrl] = useState(null);
+    const [file, setFile] = useState(null);
+  
+    const handleFileSelect = (e) => {
+      setFile(e.target.files[0]);
+    }
+  
+    const uploadToS3 = async () => {
+        if (!file) {
+          return;
+        }
+        const params = { 
+          Bucket: 'test-bucket-hymns', 
+          Key: `${Date.now()}.${file.name}`, 
+          Body: file 
+        };
+        const { Location } = await s3.upload(params).promise();
+        setImageUrl(Location);
+        console.log('uploading to s3', Location);
+    }
+  
+    return (
+      <div >
+        <h1>Test File Upload</h1>
+        <input type="file" onChange={handleFileSelect} />
+        {file && (
+          <div style={{ marginTop: '10px' }}>
+            <button onClick={uploadToS3}>Upload</button>
+          </div>
+        )}
+        {imageUrl && (
+          <div style={{ marginTop: '10px' }}>
+            <img src={imageUrl} alt="uploaded" />
+          </div>
+        )}
+      </div>
+    );
+}
